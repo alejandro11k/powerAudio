a0f6bd788b56d15f960b3765d27940a7da0dc584
alejandro11k <k11alejandro@gmail.com>
2018-09-07 08:45:01 -0300

    electron-vue init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'power-audio'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
