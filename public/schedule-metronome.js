5389b7b8f70b3f5b9c932d264cb3be291f57d642
alejandro11k <k11alejandro@gmail.com>
2018-10-09 22:48:26 -0300

    schedule funcion schedule(bpm, minutos) OK

--- public/schedule-metronome.js
+++ public/schedule-metronome.js
@@ -1,6 +1,7 @@
 export class ScheduleMetronome {
     constructor() {
         // threeTimesAt60bpm()
+        let audioContext = null
     }
 
     threeTimesAt60bpm() {
@@ -9,6 +10,13 @@ export class ScheduleMetronome {
             this.play(0, 3, 0.5, audioContext)
             this.play(1, 3, 0.5, audioContext)
             this.play(2, 3, 0.5, audioContext)
+        })
+    }
+
+    schedule(bpm, minutes) {
+        let audioContext = new AudioContext()
+        audioContext.audioWorklet.addModule('./processor.js').then(() => {
+            this.inputMinutes(bpm, minutes, audioContext)
         })
     }
     
@@ -23,6 +31,39 @@ export class ScheduleMetronome {
       
       oscillator.start(startTime)
       oscillator.stop(endTime)
+    }
+
+    inputMinutes(bpm, minutes, audioContext) {
+        let times = 1
+        const delay = this.bpm2seg(bpm)
+        let beats = this.minutes2Beats(minutes, delay)
+
+        console.log(delay)
+        console.log(beats)
+
+        for (let beat = 0; beat < beats; beat = beat+delay) {
+            this.play(beat, 3, 0.5, audioContext)
+            console.log(beat)
+            console.log(times++)
+        }
+
+        this.audioContext = audioContext
+
+    }
+
+    stop() {
+        this.audioContext.suspend()
+    }
+
+    bpm2seg(bpm) {
+        const second = (60 / bpm);
+        return second
+    }
+
+    minutes2Beats(minutes, delay) {
+        const seconds = 60 / minutes
+        const times = seconds / delay
+        return times 
     }
 
 }
