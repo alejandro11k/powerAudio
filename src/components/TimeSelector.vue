0415938bf17ecc2f49292ca29133b9a27ca93e15
alejandro11k <k11alejandro@gmail.com>
2018-10-27 00:42:13 -0300

    timeSelecto2component OK

---
+++ src/components/TimeSelector.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <br>
+      {{ timeLimitInMinutes }}
+      <br>
+      <md-button class="md-fab md-mini md-primary" @click="add10">
+        <md-icon> + </md-icon>
+      </md-button>
+      <md-button class="md-fab md-mini md-plain" @click="sub10">
+        <md-icon> - </md-icon>
+      </md-button>    
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TimeSelector',
+  data() {
+      return {
+          timeLimit: 0
+      }
+  },
+  watch: {
+      timeLimit: function (value) { this.$emit('timeLimit', value) }
+  },
+  methods: {
+    add10 () {
+      this.timeLimit = this.timeLimit + 10
+    },
+    sub10 () {
+      this.timeLimit = this.timeLimit - 10
+    },
+  },
+  computed: {
+      timeLimitInMinutes () {
+      let actualLimit = this.timeLimit
+      let value = 0
+      if (actualLimit < 60) {
+        value = actualLimit + ' seg.'
+      } else {
+        let minutos = Math.floor(actualLimit / 60)
+        let segundos = actualLimit - (minutos * 60)
+        if (segundos==0) {
+          segundos = ''
+        } else {
+          segundos = ':' + segundos
+        }
+        value = minutos + segundos + ' minute'
+      }
+      return value
+    }
+  }
+}
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,15 +34,7 @@
     </div>
 
     <div>
-      <br>
-      {{ timeLimitInMinutes() }}
-      <br>
-      <md-button class="md-fab md-mini md-primary" @click="add10">
-        <md-icon> + </md-icon>
-      </md-button>
-      <md-button class="md-fab md-mini md-plain" @click="sub10">
-        <md-icon> - </md-icon>
-      </md-button>
+      <time-selector @timeLimit="setTimeLimit($event)"></time-selector>
 
       <md-switch v-model="timeLimitEnable" class="md-primary"></md-switch>
       <div> Time Limit:
@@ -59,6 +51,7 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import SoundSelector from '@/components/SoundSelector.vue'
+import TimeSelector from '@/components/TimeSelector.vue'
 import VueKnobControl from 'vue-knob-control'
 import RangeSlider from 'vue-range-slider'
 // you probably need to import built-in style
@@ -70,14 +63,13 @@ export default {
     // HelloWorld
     'knob-control': VueKnobControl,
     'range-slider': RangeSlider,
-    'sound-selector': SoundSelector
+    'sound-selector': SoundSelector,
+    'time-selector': TimeSelector
   },
   data() {
     return {
       bpm: this.$store.state.bpm,
-      soundSelect: this.$store.state.soundSelect,
       volume: this.$store.state.volume,
-      timeLimit: this.$store.state.timeLimit,
       timeLimitEnable: this.$store.state.timeLimitEnable,
       timerValue: this.$store.state.timer.getTimeValues().toString(),
       click: false,
@@ -86,9 +78,7 @@ export default {
   },
   watch: {
     bpm: function (value) { this.setBpm(value) },
-    soundSelect: function (value) { this.setSound(value) },
     volume: function (value) { this.setGain(value) },
-    timeLimit: function (value) { this.setTimeLimit(value) },
     timeLimitEnable: function (value) { this.setTimeLimitEnable(value) },
   },
   methods: {
@@ -104,7 +94,7 @@ export default {
       const timer = this.$store.state.timer
       if (!timer.isRunning()) {
         if (this.timeLimitEnable) {
-          timer.start({countdown: true, startValues: {seconds: this.timeLimit}});
+          timer.start({countdown: true, startValues: {seconds: this.$store.state.timeLimit}});
           timer.addEventListener('targetAchieved', () => {
             this.timerValue = 'Well Done!'
           });
@@ -143,15 +133,9 @@ export default {
       // eslint-disable-next-line
       StateNodes.setGain(this.$store.state.volume)
     },
-    add10 () {
-      this.timeLimit = this.timeLimit + 10
-    },
-    sub10 () {
-      this.timeLimit = this.timeLimit - 10
-    },
+    
     setTimeLimit (value) {
       this.$store.commit('setTimeLimit', value)
-      console.log('after', this.$store.state.timeLimit)
       // eslint-disable-next-line
       setTimeLimit(this.$store.state.timeLimit)
     },
@@ -160,23 +144,7 @@ export default {
       // eslint-disable-next-line
       setTimeLimitEnable(this.$store.state.timeLimitEnable)
     },
-    timeLimitInMinutes () {
-      let actualLimit = this.$store.state.timeLimit
-      let value = 0
-      if (actualLimit < 60) {
-        value = actualLimit + ' seg.'
-      } else {
-        let minutos = Math.floor(actualLimit / 60)
-        let segundos = actualLimit - (minutos * 60)
-        if (segundos==0) {
-          segundos = ''
-        } else {
-          segundos = ':' + segundos
-        }
-        value = minutos + segundos + ' minute'
-      }
-      return value
-    }
+    
   },
   computed: {
 
