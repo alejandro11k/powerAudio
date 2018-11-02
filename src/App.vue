bdd3b6c87a07d6e04325e2df5ccb289fbd03d495
alejandro11k <k11alejandro@gmail.com>
2018-11-02 07:20:59 -0300

    longpress directive testing

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/schedule">Schedule</router-link>
+      <router-link to="/schedule">Schedule</router-link> |
+      <router-link to="/test">Test</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -3,7 +3,7 @@
     <md-button class="md-fab" @click="add">
         <md-icon> + </md-icon>
     </md-button>
-    <md-button class="md-fab" @click="play($event)">
+    <md-button class="md-fab" v-longpress="test" @click="play">
         <md-icon> > </md-icon>
     </md-button>
     
@@ -78,7 +78,8 @@ export default {
   methods: {
     test() {
       // eslint-disable-next-line
-      ScheduleModule.test()
+      // ScheduleModule.test()
+      console.log('teeeeeessst!')
     },
     deleteChip(pos) {
       const tempList = this.$store.getters.getScheduleTempList
@@ -114,14 +115,71 @@ export default {
       tempList.push(newSchedule)
       this.$store.commit('setScheduleTempList', tempList)
     },
-    play(event) {
-      console.log(event)
+    play() {
       const original = this.$store.getters.getScheduleTempList // return observer?!?!?!?!)
       let cloned = JSON.parse(JSON.stringify(original)); // this will copy everything from original 
       // eslint-disable-next-line
       ScheduleModule.suspendResume(cloned)
     }
-  }
+  },
+  directives: {
+        longpress: {
+            // https://blog.logrocket.com/building-a-long-press-directive-in-vue-3408d60fb511
+            bind: function (el, binding, vNode) {
+            // Make sure expression provided is a function
+                if (typeof binding.value !== 'function') {
+                    // Fetch name of component
+                    const compName = vNode.context.name
+                    // pass warning to console
+                    let warn = `[longpress:] provided expression '${binding.expression}' is not a function, but has to be`
+                    if (compName) { warn += `Found in component '${compName}' ` }
+
+                    console.warn(warn)
+                }
+
+                // Define variable
+                let pressTimer = null
+
+                // Define funtion handlers
+                // Create timeout ( run function after 1s )
+                let start = (e) => {
+
+                    if (e.type === 'click' && e.button !== 0) {
+                        return;
+                    }
+
+                    if (pressTimer === null) {
+                        pressTimer = setTimeout(() => {
+                            // Run function
+                            handler()
+                        }, 1000)
+                    }
+                }
+
+                // Cancel Timeout
+                let cancel = () => {
+                    // Check if timer has a value or not
+                    if (pressTimer !== null) {
+                        clearTimeout(pressTimer)
+                        pressTimer = null
+                    }
+                }
+                // Run Function
+                const handler = (e) => {
+                    binding.value(e)
+                }
+
+                // Add Event listeners
+                el.addEventListener("mousedown", start);
+                el.addEventListener("touchstart", start);
+                // Cancel timeouts if this events happen
+                el.addEventListener("click", cancel);
+                el.addEventListener("mouseout", cancel);
+                el.addEventListener("touchend", cancel);
+                el.addEventListener("touchcancel", cancel);
+            }
+        }
+    }
 }
 </script>
 
